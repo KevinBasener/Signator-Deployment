5b7a422a58b8594602528a56900468230b30a312
KevinBasener <kevin.basener@gmail.com>
2025-10-02 10:42:19 +0200

    Change title

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Inkplate',
+  description: 'Inkplate Event Management',
 }
 
 export default function RootLayout({
